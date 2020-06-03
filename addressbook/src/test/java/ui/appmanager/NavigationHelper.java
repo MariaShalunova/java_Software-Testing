1e078654a98320420a82b55ba18e9130d426f47e
Maria Shalunova <shalunova00@gmail.com>
2020-06-03 04:22:04 +0300

    лекция 2.8

--- addressbook/src/test/java/ui/appmanager/NavigationHelper.java
+++ addressbook/src/test/java/ui/appmanager/NavigationHelper.java
@@ -3,19 +3,18 @@ package ui.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    private WebDriver wd;
+public class NavigationHelper extends HelperBase{
 
     public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void goToGroupsPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void goToContactsPage() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 
 }
