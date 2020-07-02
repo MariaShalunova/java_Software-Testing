3304ee45e824d9c7e18b77da03f899bb8d9fd112
Maria Shalunova <shalunova00@gmail.com>
2020-07-02 23:43:33 +0300

    Лекция 3.6

--- addressbook/src/test/java/ui/tests/Contacts/ContactModificationTests.java
+++ addressbook/src/test/java/ui/tests/Contacts/ContactModificationTests.java
@@ -17,7 +17,7 @@ public class ContactModificationTests extends TestBase {
                 "test", "test", "test", "test",
                 "test", "10", "February", "2000",
                 "13", "February", "2000", "test", "test",
-                "test", "test"));
+                null, "test"));
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToContactsPage();
     }
