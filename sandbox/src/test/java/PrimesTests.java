4d7245b087e53e66b7c5afac7b53584de51e3acf
Maria Shalunova <shalunova00@gmail.com>
2020-07-14 05:05:14 +0300

    Лекция 4.2. Коллекции: наборы элементов

--- sandbox/src/test/java/PrimesTests.java
+++ sandbox/src/test/java/PrimesTests.java
@@ -12,4 +12,15 @@ public class PrimesTests {
     public void testNonPrime() {
         Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
     }
+
+    @Test(enabled = false)
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
+
+    @Test
+    public void testPrimeFast() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
 }
